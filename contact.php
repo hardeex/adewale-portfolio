f3017316cff312503cb686b4db883b7eccf1ac6a
Adewale <jamiuadewaleyusuf@gmail.com>
2023-12-27 12:38:37 +0100

    updating

---
+++ contact.php
@@ -0,0 +1,31 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    // Set up email variables
+    $to = "webmasterjdd@gmail.com"; 
+    $mail_subject = "Portfolio Message: $subject";
+    $mail_message = "Name: $name\n";
+    $mail_message .= "Email: $email\n";
+    $mail_message .= "Subject: $subject\n\n";
+    $mail_message .= "Message:\n$message";
+
+    // Send email
+    $mail_sent = mail($to, $mail_subject, $mail_message, $headers);
+
+    // Check if the email was sent
+    if ($mail_sent) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+} else {
+    // If the form wasn't submitted, redirect to the homepage or display an error message
+    header("Location: https://hardeex.github.io/adewale-portfolio/"); // Replace with the actual path of your homepage
+    exit;
+}
+?>
